1411ff0d49ec6c86c65a2a5a61042e1793fc52e1
NicolasSJ <nicolasdsanjuan@hotmail.com>
2021-07-07 22:23:43 -0300

    Version 1

---
+++ src/main/java/web/ServletControlador.java
@@ -0,0 +1,154 @@
+package web;
+
+import datos.ClienteDaoJDBC;
+import dominio.Cliente;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String accion = request.getParameter("accion");
+        if (accion != null) {
+            switch (accion) {
+                case "editar":
+                    this.editarCliente(request, response);
+                    break;
+                case "eliminar":
+                    this.eliminarCliente(request, response);
+                    break;
+                default:
+                    this.accionDefault(request, response);
+            }
+        } else {
+            this.accionDefault(request, response);
+        }
+    }
+
+    private void accionDefault(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Cliente> clientes = new ClienteDaoJDBC().listar();//recupera informacion de clientes
+        System.out.println("clientes = " + clientes);//
+        HttpSession sesion = request.getSession();
+        sesion.setAttribute("clientes", clientes);//coloca la info al alcance del REQUEST
+        sesion.setAttribute("totalClientes", clientes.size());
+        sesion.setAttribute("saldoTotal", this.calcularSaldoTotal(clientes));
+        //envia la info al JSP de clientes
+        //request.getRequestDispatcher("clientes.jsp").forward(request, response);
+
+        //manera para que no cambie el url
+        response.sendRedirect("clientes.jsp");
+    }
+
+    private double calcularSaldoTotal(List<Cliente> clientes) {
+        double saldoTotal = 0;
+        for (Cliente cliente : clientes) {
+            saldoTotal += cliente.getSaldo();
+        }
+        return saldoTotal;
+    }
+
+    private void editarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //recuperamos el idCliente
+        int idCliente = Integer.parseInt(request.getParameter("idCliente"));        
+        Cliente cliente = new ClienteDaoJDBC().encontrar(new Cliente(idCliente));
+        request.setAttribute("cliente", cliente);
+        String jspEditar = "/WEB-INF/paginas/cliente/editarCliente.jsp";
+        request.getRequestDispatcher(jspEditar).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String accion = request.getParameter("accion");
+        if (accion != null) {
+            switch (accion) {
+                case "insertar":
+                    this.insertarCliente(request, response);
+                    break;
+                case "editar":
+                    this.editarCliente(request, response);
+                    break;
+                case "modificar":
+                    this.modificarCliente(request, response);
+                    break;
+                default:
+                    this.accionDefault(request, response);
+            }
+        } else {
+            this.accionDefault(request, response);
+        }
+    }
+
+    private void insertarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //recuperar los valores del formulario agregarCliente
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String telefono = request.getParameter("telefono");
+        String email = request.getParameter("email");
+        double saldo = 0;
+        String saldoString = request.getParameter("saldo");
+        if (saldoString != null && !"".equals(saldoString)) {
+            saldo = Double.parseDouble(saldoString);
+        }
+
+        //creamos el objeto cliente (modelo)
+        Cliente cliente = new Cliente(nombre, apellido, email, telefono, saldo);
+
+        //insertamos el nuevo objeto en la base de datos
+        int registrosModificados = new ClienteDaoJDBC().insertar(cliente);
+        System.out.println("registrosModificados = " + registrosModificados);
+
+        //redirigimos a la accion por default
+        this.accionDefault(request, response);
+    }
+    
+    private void modificarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //recuperar los valores del formulario editarCliente
+        int idCliente = Integer.parseInt(request.getParameter("idCliente"));
+        String nombre = request.getParameter("nombre");
+        String apellido = request.getParameter("apellido");
+        String telefono = request.getParameter("telefono");
+        String email = request.getParameter("email");
+        double saldo = 0;
+        String saldoString = request.getParameter("saldo");
+        if (saldoString != null && !"".equals(saldoString)) {
+            saldo = Double.parseDouble(saldoString);
+        }
+
+        //creamos el objeto cliente (modelo)
+        Cliente cliente = new Cliente(idCliente, nombre, apellido, email, telefono, saldo);
+
+        //modificar el objeto existente en la base de datos
+        int registrosModificados = new ClienteDaoJDBC().actualizar(cliente);
+        System.out.println("registrosModificados = " + registrosModificados);
+
+        //redirigimos a la accion por default
+        this.accionDefault(request, response);
+    }
+    
+    private void eliminarCliente(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //recuperar los valores del formulario editarCliente
+        int idCliente = Integer.parseInt(request.getParameter("idCliente"));
+
+        //creamos el objeto cliente (modelo)
+        Cliente cliente = new Cliente(idCliente);
+
+        //eliminamos el objeto existente en la base de datos
+        int registrosModificados = new ClienteDaoJDBC().eliminar(cliente);
+        System.out.println("registrosModificados = " + registrosModificados);
+
+        //redirigimos a la accion por default
+        this.accionDefault(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/paginas/comunes/piePagina.jsp
@@ -0,0 +1,10 @@
+<footer id="pie-pagina" class="bg-warning text-white mt-5 p-5">
+    <div class="container">
+        <div class="cool">
+            <p class="lead text-center">
+                Copyright &copy; GlobalMentory
+            </p>
+        </div>
+    </div>
+
+</footer>
